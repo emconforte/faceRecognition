e285a253342ab992596afd71d1cf8b9872f30b1e
emconforte <35504364+emconforte@users.noreply.github.com>
2020-11-14 15:15:11 -0600

    added form

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import face from './faceIcon.png'
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> 👽 </div>
+                <div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='logo' src={face}/> </div>
             </Tilt>
         </div>
     );
