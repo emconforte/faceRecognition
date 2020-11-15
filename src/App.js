ea336babc72f6e341bb5131014d0a5d60945e615
emconforte <35504364+emconforte@users.noreply.github.com>
2020-11-14 21:03:33 -0600

    added input functionality to detect button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
 
+const app = new Clarifai.App({
+  apiKey: '8d37e736ba724255a681410325195a36'
+ });
+
+const particlesOptions = {
+  particles: {
+    number: {
+      density: {
+        enable: true,
+        value_area: 800
+      },
+      value: 100
+    }
+  }
+}
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    console.log('click');
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+      },
+      function (err) {
+        //do something with error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
-        {/* 
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
