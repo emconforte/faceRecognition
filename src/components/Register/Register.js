1d5ae66dbb5fa6c8c932048bf3c63b972132d586
emconforte <35504364+emconforte@users.noreply.github.com>
2020-11-24 20:34:37 -0600

    fetch changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/register', {
+        fetch('https://floating-shelf-97513.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
